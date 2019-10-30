6507c721ebe74ae91255cb9c670782d126189346
Taewoo Kim <ktw5780264@outlook.kr>
2019-10-30 15:52:58 +0900

    2019-10-30 5~6교시 풀이

--- 1079/1079.cpp
+++ 1079/1079.cpp
@@ -1,10 +1,15 @@
-﻿#include<stdio.h>
-int main() {
-	char x;
-	scanf_s("%c", &x);
-	while (x != 'q') {
-		printf("%c", x);
-		scanf_s("%c", &x);
+﻿#include <stdio.h>
+int main()
+{
+	int a;
+aaa:
+	scanf("%c", &a);
+	if (a != 'q', a != ' ')
+	{
+		printf("%c\n", a);
+		goto aaa;
 	}
-	return 0;
+	else goto bbb;
+bbb: if (a == 'q') printf("q");
+	if (a == ' ') goto aaa;
 }
---
+++ 1080/1080.cpp
@@ -0,0 +1,10 @@
+﻿#include<stdio.h>
+int main() {
+	int sum = 0;
+	int i, n;
+	scanf_s("%d", &n);
+	for (i = 0; i <= n; i++){
+		
+	}
+	return 0;
+}
