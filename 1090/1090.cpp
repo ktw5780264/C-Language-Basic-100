11ea81d8ed8a4f67e26c6dff135cb266cf71d7fa
Taewoo Kim <ktw5780264@outlook.kr>
2019-10-31 10:25:21 +0900

    2019-10-31 2교시 풀이

---
+++ 1090/1090.cpp
@@ -0,0 +1,10 @@
+﻿#include<stdio.h>
+int main() {
+	long long int a, r, n, i;
+	scanf_s("%lld %lld %lld", &a, &r, &n);
+		for (i = 1; i < n; i++) {
+			a *= r;
+		}
+		printf("%lld", a);
+	return 0;
+}
