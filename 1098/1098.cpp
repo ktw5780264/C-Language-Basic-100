d7e4ccfba96da2d1f7d83015ece0ea4a6222238a
Taewoo Kim <ktw5780264@outlook.kr>
2019-10-31 14:42:07 +0900

    2019-10-31 5교시 풀이

---
+++ 1098/1098.cpp
@@ -0,0 +1,31 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int h, w, n, l, d, x, y, i, j;
+	int a[101][101] = { 0 };
+	scanf_s("%d %d", &h, &w);
+	scanf_s("%d", &n);
+
+	for (i = 1; i <= n; i++){
+		scanf_s("%d %d %d %d", &l, &d, &x, &y);
+		if (l == 1)
+			if (a[x][y] == 0) a[x][y] = 1;
+		if (l != 1){
+			if (d == 0)
+				for (j = 1; j <= l; j++)
+					a[x][y + j - 1] = 1;
+			else if (d == 1)
+				for (j = 1; j <= l; j++)
+					a[x + j - 1][y] = 1;
+		}
+	}
+
+	for (i = 1; i <= h; i++){
+		for (j = 1; j <= w; j++){
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
